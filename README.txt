1ef4d777a26ed6edbd75f3f4c2630f98d8b266dc
kgarri <black.hat.kg@gmail.com>
2024-11-30 17:29:42 -0600

    Storing these new changes

--- README.txt
+++ README.txt
@@ -12,6 +12,6 @@ To run on Windows:
 1. Have LLVM and Clang++ installed (installation with Msys2 package manager is easiest) 
 2. Open Msys2 MinGW64 terminal 
 3. Run the following command in the Grok directory to compile to k.exe: 
-  clang++ -v -g main.cpp lexer.cpp parser.cpp codegen.cpp toplevel.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit native` -fuse-ld=lld -o k
+  clang++ -Xlinker --export-dynamic -v -g main.cpp lexer.cpp parser.cpp codegen.cpp toplevel.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit native` -fuse-ld=lld -o k
 4. Use this command to run: 
   start k.exe
--- main.cpp
+++ main.cpp
@@ -1,40 +1,21 @@
 #include "KaleidoscopeJIT.h"
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/PassManager.h"
-#include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
-#include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/StandardInstrumentations.h"
 #include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Transforms/InstCombine/InstCombine.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/Transforms/Scalar/GVN.h"
-#include "llvm/Transforms/Scalar/Reassociate.h"
-#include "llvm/Transforms/Scalar/SimplifyCFG.h"
-#include <algorithm>
 #include <cassert>
 #include <cctype>
-#include <cstdint>
 #include <cstdio>
+#include <cstring>
 #include <cstdlib>
 #include <map>
 #include <memory>
-#include <string>
-#include <vector>
 
-#include "lexer.h"
 #include "parser.h"
 #include "codegen.h"
-#include "ast.h"
 #include "toplevel.h"
 
 using namespace std;
@@ -54,35 +35,62 @@ using namespace llvm::orc;
 
 // on windows, export the functions because dynamic symbol loader will use
 // GetProcAddress to find symbols
-#ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
+#if defined(_MSC_VER)
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
 #else
-#define DLLEXPORT
+    #define EXPORT
+    #define IMPORT
 #endif
+
+
 
 // PRODUCE CONSOLE OUTPUT USING C ----
 
 // putchard - putchar takes a double ASCII value and returns 0
-// writes a single char from ASCII to stdout
-extern "C" DLLEXPORT double putchard(double X)
+// writes a single char from ASCII to stdout 
+
+extern "C" EXPORT double putchard(double X)
 {
     fputc((char)X, stderr);
     return 0;
 }
 
+
 // printd - printf that takes a double and prints with new line, returns 0
-extern "C" DLLEXPORT double printd(double X)
+extern "C" EXPORT double printd(double X)
 {
     fprintf(stderr, "%f\n", X);
     return 0;
 }
 
 // printstr - printf that takes string and prints with newline, returns 0
-extern "C" DLLEXPORT char printstr(char S[])
+extern "C" EXPORT char printstr(char S[])
 {
     fprintf(stderr, "%s\n", S);
     return 0;
 }
+
+extern "C" EXPORT char* concatstr(char S1[], char S2[])
+{
+     int lengthOfStr1 = strlen(S1);
+     int lengthOfStr2 = strlen(S2);
+     char *result = (char*)malloc(lengthOfStr1 + lengthOfStr2);
+     int i= 0;
+     printf("%s\n", S1);
+     while (S1[i] != '\0') {
+        if (result[i] == '\0') {
+            result[i] = S1[i];
+        }
+        i++;
+    }
+     strcat(result, S2);
+     return result;
+}
+
 
 // ----------------------------------------------------------------------------------------------
 // ==DRIVER CODE ===================================================================================
